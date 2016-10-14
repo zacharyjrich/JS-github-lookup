b574623832dd7ef16a929078849923d4d7b2e344
Zachary Richards <zachary.j.richards@gmail.com>
2016-10-14 13:14:12 -0700

    add for loop to getRepo prototype to turn object into string in order pull out name and description info from repositories

--- js/github-lookup.js
+++ js/github-lookup.js
@@ -7,16 +7,15 @@ Repo.prototype.getUser = function(user) {
   $.get('https://api.github.com/users/' + user + '?access_token=' + apiKey).then(function(response){
     $('#showResults').text(user);
     // console.log(JSON.stringify(response))
-    console.log(user, response);
   }).fail(function(error){
     $('#showResults').text(error.responseJSON.message);
   });
 }
 
-Repo.prototype.getRepos = function(user, repos) {
+Repo.prototype.getRepos = function(user) {
   $.get('https://api.github.com/users/' + user + '/repos?access_token=' + apiKey).then(function(response){
     for(var i = 0; i < response.length; i++) {
-      console.log(repos, response[i].name, response[i].description);
+      console.log(user, response[i].name, response[i].description);
     }
     // $('#showResults').text(user);
     // console.log(JSON.stringify(response))
