83ab369b9761640ca3c1209a652a413f8a0d1a92
Zachary Richards <zachary.j.richards@gmail.com>
2016-10-14 11:16:31 -0700

    add to .get to show user info in console to make sure token is passing correctly

--- js/github-lookup-interface.js
+++ js/github-lookup-interface.js
@@ -1,11 +1,15 @@
 var Repo = require('./../js/github-lookup.js').reposModule;
 
+// var displayRepNames = function(user, repName) {
+//   $('#showResults').text("Rep Names are" + repName);
+// }
+
 $(document).ready(function() {
   var currentUserObject = new Repo();
   $('#getResults').click(function() {
     var userName = $('#user').val();
     $('#user').val("");
     $('#showResults').text('blah');
-    currentUserObject.getRepos();
+    currentUserObject.getRepos(userName);
   });
 });
--- js/github-lookup.js
+++ js/github-lookup.js
@@ -3,11 +3,12 @@ var apiKey = require('./../.env').apiKey;
 Repo = function(){
 }
 
-Repo.prototype.getRepos = function() {
-    $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+Repo.prototype.getRepos = function(user) {
+    $.get('https://api.github.com/users/' + user + '/repos?access_token=' + apiKey).then(function(response){
+      $('#showResults').text(user);
       console.log(response);
     }).fail(function(error){
-      console.log(error.responseJSON.message);
+      $('#showResults').text(error.responseJSON.message);
     });
 }
 
